4a98d4e1cdf254f97a041c6b6d5ea96cbad19111
Oscillate <liujerry1million@yahoo.com>
2016-05-15 00:07:50 -0400

    Created project

---
+++ Assets/Scripts/CircleBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleBehaviour : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public int vertSpeed;
+
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("w")) {
+			rb.AddForce (transform.up * vertSpeed);
+		}
+		if (Input.GetKey ("s")) {
+			rb.AddForce (transform.up * -vertSpeed);
+		}
+	}
+}
