e7dffe836861fa99841216b6e73e2861ab199537
Oscillate <liujerry1million@yahoo.com>
2016-05-15 02:12:14 -0400

    Simple movement

--- Assets/Scripts/CircleBehaviour.cs
+++ Assets/Scripts/CircleBehaviour.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class CircleBehaviour : MonoBehaviour {
 
 	private Rigidbody2D rb;
-	public int vertSpeed;
+	public Vector2 Speed;
 
 	// Use this for initialization
 	void Start () {
@@ -14,10 +14,21 @@ public class CircleBehaviour : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey ("w")) {
-			rb.AddForce (transform.up * vertSpeed);
+			rb.AddForce (Vector2.up * Speed.x);
 		}
 		if (Input.GetKey ("s")) {
-			rb.AddForce (transform.up * -vertSpeed);
+			rb.AddForce (Vector2.up * -Speed.x);
 		}
+		if (Input.GetKey ("a")) {
+			rb.AddForce (Vector2.right * -Speed.y);
+		}
+		if (Input.GetKey ("d")) {
+			rb.AddForce (Vector2.right * Speed.y);
+		}
+
+		Vector3 mousePosReal = Input.mousePosition;
+		mousePosReal.z = 10;
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(mousePosReal);
+		rb.transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
 	}
 }
