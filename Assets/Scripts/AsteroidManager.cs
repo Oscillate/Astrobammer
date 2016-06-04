02460ca036192a7fd6c53479777ca59d3ce57164
Oscillate <liujerry1million@yahoo.com>
2016-06-04 14:52:05 -0400

    asteroid spawning

--- Assets/Scripts/AsteroidManager.cs
+++ Assets/Scripts/AsteroidManager.cs
@@ -11,7 +11,9 @@ public class AsteroidManager : MonoBehaviour {
 	public float timeBetweenSpawns;
 
 	private float lastSpawn;
-	 
+
+	public float generativeOffset;
+
 	void Awake(){
 		Asteroids = new List<Asteroid> ();
 	}
@@ -30,8 +32,33 @@ public class AsteroidManager : MonoBehaviour {
 	}
 
 	void SpawnAsteroid(){
-		GameObject newAsteroid = Instantiate (asteroidPrefab, Vector3.zero, Quaternion.identity) as GameObject;
-		float newAngle = Random.value * 2 * Mathf.PI;
-		newAsteroid.GetComponent<Rigidbody2D> ().velocity = new Vector2 (Mathf.Cos (newAngle) * initialSpeed, Mathf.Sin (newAngle) * initialSpeed);
+		
+		Vector3 screenLocation, worldLocation;
+		screenLocation = Vector3.zero;
+		screenLocation.x = Random.value;
+		screenLocation.y = Random.value;
+		int edge = Random.Range (0, 4);
+		switch (edge) {
+		case 0:
+			screenLocation.x = 0 - generativeOffset;
+		break;
+			case 1:
+			screenLocation.y = 0 - generativeOffset;
+		break;
+			case 2:
+			screenLocation.x = 1 + generativeOffset;
+		break;
+			case 3:
+			screenLocation.y = 1 + generativeOffset;
+		break;
+		}
+		worldLocation = Camera.main.ViewportToWorldPoint(screenLocation);
+		worldLocation.z = 0;
+		GameObject newAsteroid = Instantiate (asteroidPrefab, worldLocation, Quaternion.identity) as GameObject;
+		screenLocation = Vector3.zero;
+		screenLocation.x = Random.value;
+		screenLocation.y = Random.value;
+		worldLocation = Camera.main.ViewportToWorldPoint(screenLocation);
+		newAsteroid.GetComponent<Rigidbody2D> ().velocity = Vector3.Normalize(worldLocation - newAsteroid.transform.position) * initialSpeed;
 	}
 }
