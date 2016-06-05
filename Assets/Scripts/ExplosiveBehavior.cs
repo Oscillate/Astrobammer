63da5c4ba490f348509907a26bb5967031c4d7b9
Oscillate <liujerry1million@yahoo.com>
2016-06-05 14:03:11 -0400

    Tab fixing

--- Assets/Scripts/ExplosiveBehavior.cs
+++ Assets/Scripts/ExplosiveBehavior.cs
@@ -1,31 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ExplosiveBehavior : MonoBehaviour {
-        public int detonateTimer = 100;
+public class ExplosiveBehavior : MonoBehaviour
+{
+    public int detonateTimer = 100;
     public GameObject explosion;
-        // Use this for initialization
-        void Start () {
-        }
+    // Use this for initialization
+    void Start ()
+    {
+    }
 
-        void explode() {
-                Instantiate(explosion, this.transform.position, this.transform.rotation);
-                Destroy(gameObject);
-        }
+    void explode ()
+    {
+        Instantiate (explosion, this.transform.position, this.transform.rotation);
+        Destroy (gameObject);
+    }
 
-        void OnTriggerEnter2D(Collider2D c){
-                if (c.sharedMaterial.name.Equals("BatHitBox")) {
-                        this.GetComponent<Rigidbody2D> ().AddForce ( 6000 * c.gameObject.transform.up);
-                }
+    void OnTriggerEnter2D (Collider2D c)
+    {
+        if (c.sharedMaterial.name.Equals ("BatHitBox")) {
+            this.GetComponent<Rigidbody2D> ().AddForce (6000 * c.gameObject.transform.up);
         }
+    }
 
-        // Update is called once per frame
-        void Update () {
-                if (detonateTimer > 0) {
-                    detonateTimer--;
-                }
-                else {
-                    explode();
-                }
+    // Update is called once per frame
+    void Update ()
+    {
+        if (detonateTimer > 0) {
+            detonateTimer--;
+        } else {
+            explode ();
         }
+    }
 }
